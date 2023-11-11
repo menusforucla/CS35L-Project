4a322a3eb2c9fa888c548f0b59d54711bd729632
Seif <seifaziz10@gmail.com>
2023-11-11 11:18:10 -0800

    Update metadata for app

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -20,19 +20,8 @@ const fontSans = Inter({
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
-  openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
-  },
+  title: "Menus for UCLA",
+  description: "Menus for UCLA",
 };
 
 export default function Layout(props: { children: React.ReactNode }) {
