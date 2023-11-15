79126fc312ede73017f292089ab5a9499acf7f34
Umair Khan <umairs0607@gmail.com>
2023-11-14 22:19:34 -0800

    home page for dining halls Basic layout for dining hall home page alongside placeholder titles and images Co-authored-by: Seif <seifaziz10@gmail.com>

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-
 import { AuthShowcase } from "./_components/auth-showcase";
 import {
   CreatePostForm,
@@ -7,7 +6,32 @@ import {
   PostList,
 } from "./_components/posts";
 
-// export const runtime = "edge";
+import Image from "next/image";
+import Link from 'next/link';
+interface DiningHallProps {
+  title: string;
+  imageUrl: string;
+  availability: number;
+}
+const DiningHall: React.FC<DiningHallProps> = ({title, imageUrl, availability}) => {
+  return(
+    <Link href={{pathname: "/dining-hall", query: {title: title}}}>
+      <div className="w-full h-full rounded overflow-hidden shadow-lg text-center bg-white p-10">
+        <h1 className="text-5xl font-bold mb-2">{title}</h1>
+        <div className="w-100 h-72 relative items-center justify-center mx-auto">
+          <Image 
+            className="m-auto object-cover rounded-t" 
+            src={imageUrl} 
+            alt={title}
+            layout="fill"
+          />
+        </div>
+        <h2 className="text-xl font-bold mt-2">Availability: {availability}%</h2>
+      </div>
+      </Link>
+  );
+};
+
 
 export default function HomePage() {
   return (
@@ -18,6 +42,12 @@ export default function HomePage() {
         </h1>
         <AuthShowcase />
 
+        <div className="flex w-full justify-around">
+          <DiningHall title="Krusty Krab" imageUrl="/images/KrustyKrab.webp" availability={100}/>
+          <DiningHall title="Krusty Krab 2" imageUrl="/images/KrustyKrab2.webp" availability={20}/>
+          <DiningHall title="Chum Bucket" imageUrl="/images/ChumBucket.webp" availability={0}/>
+
+        </div>
         {/* <CreatePostForm /> */}
         {/* <div className="h-[40vh] w-full max-w-2xl overflow-y-scroll">
           <Suspense
@@ -34,3 +64,5 @@ export default function HomePage() {
     </main>
   );
 }
+
+
