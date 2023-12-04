2200c0b5f6fde9f6e4b84f08845e3b42cfd43899
Kai Wang <kaiwsv@gmail.com>
2023-12-03 18:23:25 -0800

    completed clean version of dining scraper in TypeScript

--- scraping/scraper.ts
+++ scraping/scraper.ts
@@ -3,6 +3,7 @@ import * as cheerio from 'cheerio';
 import { default as axios } from 'axios';
 import { AxiosResponse } from 'axios';
 import * as fs from 'fs';
+import { LargeNumberLike } from 'crypto';
 
 //dining urls
 let diningURLs: string[] = [
@@ -31,10 +32,18 @@ type MenuItem = {
 interface NutritionalInfo {
     servingSize: string;
     caloriesPerServing: string;
-    macronutrients: string[];
-    vitamins: string[];
+    macronutrients: macro[];
+    vitamins: [string, number][];
     ingredients: string[];
+    allergens: string[]
     options: string[];
+}
+
+interface macro {
+    name: string
+    grams: string
+    dv: number
+    indented: boolean
 }
 
 //scrape dining hall availability
@@ -44,10 +53,10 @@ async function scrapeHours(): Promise<void> {
             .then(({ data }) => {
                 const $ = cheerio.load(data);
                 const activityData: ActivityData[] = [];
-                $('table.hours-table tr').each((index, element) => {
+                $('table.hours-table tr').map((index, element) => {
                     const name = $(element).find('span.hours-location').text();
                     const activity = $(element).find('span.activity-level-box').text().trim();
-                    if (activity != '' && !isNaN(Number(activity.slice(0, -1)))) {
+                    if (activity != '' && !isNaN(parseInt(activity.slice(0, -1), 10))) {
                         const activityLevel: ActivityData = {
                             name: name,
                             percentage: parseInt(activity.slice(0, -1), 10)
@@ -80,15 +89,12 @@ async function scrapeMenu(menuOption: number): Promise<void> {
                     name: $(item).text(),
                     section: sectionName,
                     link: String($(item).attr('href')),
-                    nutrition: await nutritionScraper(String($(item).attr('href')))
+                    nutrition: await scrapeNutrition(String($(item).attr('href')))
                 };
                 menuItems.push(menuItem);
             }
         }
-
-        for (const item of menuItems) {
-        //    console.log(item);
-        }
+        console.log(menuItems);
 
     }
     catch (error) {
@@ -96,7 +102,7 @@ async function scrapeMenu(menuOption: number): Promise<void> {
     }
 }
 
-async function nutritionScraper(url: string): Promise<NutritionalInfo> {
+async function scrapeNutrition(url: string): Promise<NutritionalInfo> {
     try {
         const html: AxiosResponse<string> = await axios.get(url);
 
@@ -106,27 +112,49 @@ async function nutritionScraper(url: string): Promise<NutritionalInfo> {
 
         const $ = cheerio.load(html.data);
 
+        //scrape serving size
+        //const servingSize = $('.nfserv').text().slice(13).trim();
+        //scrape calories per serving
+        //const caloriesPerServing = $('.nfcal').text().slice(9).trim();
         //scrape ingredients
-        const ingredients = [];
-        const ingredientsString = $('.ingred_allergen').find('p').first().text().slice(13);
-       // console.log(ingredientsString.split('/,\s*(?![^()]*\))/'));
-
+        //const ingredientsString = $('.ingred_allergen').find('p').first().text().slice(13);
+        //const ingredients = ingredientsString.split(/\s*,\s*(?![^()]*\))/);
+        //scrape allergens
+        //splits ingredients by using a negative lookahead assertion to check for close paren
+        //const allergens = $('.ingred_allergen > p').eq(1).text().slice(12).split(/\s*,\s*(?![^()]*\))/);
         //scrape options
-        const options = [];
-        $('.prodwebcode').each((index,element) => {
-            console.log(element.text());
-        })
+        //const options = $('.prodwebcode').map((index, element) => $(element).text().trim()).get();
         
+        //scrape vitamins
+        const vitamins: [string, number][] = [];
+        $('.nfvitleft, .nfvitright').map((index, element) => {
+            vitamins.push([$(element).find('.nfvitname').text().trim(), parseInt($(element).find('.nfvitpct').text().trim().slice(0,-1))]);
+        })
 
-
+        const macros: macro[] = [];
+        $('.nfnutrient').map((index, element) => {
+            const dailyValue = parseInt($(element).find('.nfdvvalnum').text().trim());
+            const fullText = $(element).text().split(' ');
+            const name = fullText.slice(0, -2).join(' ');
+            const grams = fullText.slice(-2, -1).join(' ');
+            const indented = $(element).parent('.nfindent').length > 0;
+            const macro : macro = {
+                name: name, 
+                grams: grams, 
+                dv: dailyValue, 
+                indented: indented
+            };
+            macros.push(macro);
+        })
 
         const nutrition: NutritionalInfo = {
-            servingSize: "-1",
-            caloriesPerServing: "-1",
-            macronutrients: [""],
-            vitamins: [""],
-            ingredients: [""],
-            options: [""]
+            servingSize: $('.nfserv').text().slice(13).trim(),
+            caloriesPerServing: $('.nfcal').text().slice(9).trim(),
+            macronutrients: macros,
+            vitamins: vitamins,
+            ingredients: $('.ingred_allergen').find('p').first().text().slice(13).split(/\s*,\s*(?![^()]*\))/),
+            allergens: $('.ingred_allergen > p').eq(1).text().slice(12).split(/\s*,\s*(?![^()]*\))/),
+            options: $('.prodwebcode').map((index, element) => $(element).text().trim()).get()
         };
         return nutrition;
     } catch (error) {
@@ -137,9 +165,8 @@ async function nutritionScraper(url: string): Promise<NutritionalInfo> {
 
 
 //scrapeHours();
-scrapeMenu(1)
-
-
+scrapeMenu(1);
+//scrapeNutrition("http://menu.dining.ucla.edu/Recipes/979336/1"); // for debugging
 
 //checking for completion
 
