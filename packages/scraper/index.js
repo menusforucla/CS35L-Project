59bbd04f16a57846b04f6b41a617121b11bb3be8
Seif <seifaziz10@gmail.com>
2023-12-05 09:08:57 -0800

    Add tsc config

--- packages/scraper/index.js
+++ packages/scraper/index.js
@@ -1,138 +1,91 @@
-import axios, { AxiosError } from "axios";
-import * as cheerio from "cheerio";
-import { z } from "zod";
-
-import {
-  CarbonFootprint,
-  DietaryPreferenceEnum,
-  FoodAllergen,
-} from "@menus-for-ucla/db";
-
-const hoursURL = "http://menu.dining.ucla.edu/Hours";
-
-const diningURLs: string[] = [
-  "http://menu.dining.ucla.edu/Menus/DeNeve/Breakfast",
-  "http://menu.dining.ucla.edu/Menus/DeNeve/Lunch",
-  "http://menu.dining.ucla.edu/Menus/DeNeve/Dinner",
-  "http://menu.dining.ucla.edu/Menus/BruinPlate/Breakfast",
-  "http://menu.dining.ucla.edu/Menus/BruinPlate/Lunch",
-  "http://menu.dining.ucla.edu/Menus/BruinPlate/Dinner",
-  "http://menu.dining.ucla.edu/Menus/Epicuria/Dinner",
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.scrapeHours = void 0;
+var axios_1 = require("axios");
+var cheerio = require("cheerio");
+var zod_1 = require("zod");
+var db_1 = require("@menus-for-ucla/db");
+var hoursURL = "http://menu.dining.ucla.edu/Hours";
+var diningURLs = [
+    "http://menu.dining.ucla.edu/Menus/DeNeve/Breakfast",
+    "http://menu.dining.ucla.edu/Menus/DeNeve/Lunch",
+    "http://menu.dining.ucla.edu/Menus/DeNeve/Dinner",
+    "http://menu.dining.ucla.edu/Menus/BruinPlate/Breakfast",
+    "http://menu.dining.ucla.edu/Menus/BruinPlate/Lunch",
+    "http://menu.dining.ucla.edu/Menus/BruinPlate/Dinner",
+    "http://menu.dining.ucla.edu/Menus/Epicuria/Dinner",
 ];
-
-const NutritionFactsSchema = z.object({
-  servingSize: z.string(),
-  calories: z.string(),
-  totalFat: z.string(),
-  saturatedFat: z.string(),
-  transFat: z.string(),
-  cholesterol: z.string(),
-  sodium: z.string(),
-  totalCarbohydrate: z.string(),
-  dietaryFiber: z.string(),
-  sugars: z.string(),
-  protein: z.string(),
-  calcium: z.string(),
-  iron: z.string(),
-  potassium: z.string(),
-  vitaminD: z.string(),
+var NutritionFactsSchema = zod_1.z.object({
+    servingSize: zod_1.z.string(),
+    calories: zod_1.z.string(),
+    totalFat: zod_1.z.string(),
+    saturatedFat: zod_1.z.string(),
+    transFat: zod_1.z.string(),
+    cholesterol: zod_1.z.string(),
+    sodium: zod_1.z.string(),
+    totalCarbohydrate: zod_1.z.string(),
+    dietaryFiber: zod_1.z.string(),
+    sugars: zod_1.z.string(),
+    protein: zod_1.z.string(),
+    calcium: zod_1.z.string(),
+    iron: zod_1.z.string(),
+    potassium: zod_1.z.string(),
+    vitaminD: zod_1.z.string(),
 });
-
-const MenuItemSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  ingredients: z.string(),
-  allergens: z.array(z.nativeEnum(FoodAllergen)),
-  carbonFootprint: z.nativeEnum(CarbonFootprint),
-  dietaryPreferences: z.array(z.nativeEnum(DietaryPreferenceEnum)),
-  nutritionFacts: NutritionFactsSchema,
+var MenuItemSchema = zod_1.z.object({
+    name: zod_1.z.string(),
+    description: zod_1.z.string(),
+    ingredients: zod_1.z.string(),
+    allergens: zod_1.z.array(zod_1.z.nativeEnum(db_1.FoodAllergen)),
+    carbonFootprint: zod_1.z.nativeEnum(db_1.CarbonFootprint),
+    dietaryPreferences: zod_1.z.array(zod_1.z.nativeEnum(db_1.DietaryPreferenceEnum)),
+    nutritionFacts: NutritionFactsSchema,
 });
-
-const MenuSectionSchema = z.object({
-  name: z.string(),
-  menuItems: z.array(MenuItemSchema),
+var MenuSectionSchema = zod_1.z.object({
+    name: zod_1.z.string(),
+    menuItems: zod_1.z.array(MenuItemSchema),
 });
-
-const RestaurantsSchema = z.array(
-  z.object({
-    name: z.string(),
-    menuSections: z.array(MenuSectionSchema),
-    isResidentialRestaurant: z.boolean(),
-  }),
-);
-
-const ActivityLevelsSchema = z.array(
-  z.object({
-    activityLevel: z.number().min(0).max(100),
-    restaurantName: z.string(),
-  }),
-);
-
-interface ActivityData {
-  name: string;
-  percentage: number;
-}
-
-type MenuItem = {
-  name: string;
-  section: string;
-  link: string;
-  nutrition: NutritionalInfo;
-};
-
-interface NutritionalInfo {
-  servingSize: string;
-  caloriesPerServing: string;
-  macronutrients: macro[];
-  vitamins: [string, number][];
-  ingredients: string[];
-  allergens: string[];
-  options: string[];
-}
-
-interface macro {
-  name: string;
-  grams: string;
-  dv: number;
-  indented: boolean;
-}
-
+var RestaurantsSchema = zod_1.z.array(zod_1.z.object({
+    name: zod_1.z.string(),
+    menuSections: zod_1.z.array(MenuSectionSchema),
+    isResidentialRestaurant: zod_1.z.boolean(),
+}));
+var ActivityLevelsSchema = zod_1.z.array(zod_1.z.object({
+    activityLevel: zod_1.z.number().min(0).max(100),
+    restaurantName: zod_1.z.string(),
+}));
 //scrape dining hall availability
-export function scrapeHours() {
-  axios
-    .get(hoursURL)
-    .then((response) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      const $ = cheerio.load(response.data);
-      const activityData: ActivityData[] = [];
-
-      $("table.hours-table tr").map((index, element) => {
-        const name = $(element).find("span.hours-location").text().trim();
-        const activity = $(element)
-          .find("span.activity-level-box")
-          .text()
-          .trim();
-
-        const percentage = parseInt(activity.slice(0, -1), 10);
-        if (name && !isNaN(percentage)) {
-          activityData.push({ name, percentage });
-        }
-      });
-
-      console.log(activityData);
+function scrapeHours() {
+    axios_1.default
+        .get(hoursURL)
+        .then(function (response) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        var $ = cheerio.load(response.data);
+        var activityData = [];
+        $("table.hours-table tr").map(function (index, element) {
+            var name = $(element).find("span.hours-location").text().trim();
+            var activity = $(element)
+                .find("span.activity-level-box")
+                .text()
+                .trim();
+            var percentage = parseInt(activity.slice(0, -1), 10);
+            if (name && !isNaN(percentage)) {
+                activityData.push({ name: name, percentage: percentage });
+            }
+        });
+        console.log(activityData);
     })
-    .catch((error: AxiosError) => {
-      console.log(`Error fetching data from ${hoursURL}: ${error.message}`);
+        .catch(function (error) {
+        console.log("Error fetching data from ".concat(hoursURL, ": ").concat(error.message));
     });
 }
-
+exports.scrapeHours = scrapeHours;
 // async function scrapeMenu(menuOption: number): Promise<void> {
 //   try {
 //     const html: AxiosResponse<string> = await axios.get(diningURLs[menuOption]);
 //     if (html.status !== 200) {
 //       throw new Error(`Error fetching data from ${diningURLs[menuOption]}`);
 //     }
-
 //     const $ = cheerio.load(html.data);
 //     const menuItems: MenuItem[] = [];
 //     const sections = $("li.sect-item").toArray();
@@ -159,17 +112,13 @@ export function scrapeHours() {
 //     );
 //   }
 // }
-
 // async function scrapeNutrition(url: string): Promise<NutritionalInfo> {
 //   try {
 //     const html: AxiosResponse<string> = await axios.get(url);
-
 //     if (html.status !== 200) {
 //       throw new Error(`Error fetching data from ${url}`);
 //     }
-
 //     const $ = cheerio.load(html.data);
-
 //     //scrape serving size
 //     //const servingSize = $('.nfserv').text().slice(13).trim();
 //     //scrape calories per serving
@@ -182,7 +131,6 @@ export function scrapeHours() {
 //     //const allergens = $('.ingred_allergen > p').eq(1).text().slice(12).split(/\s*,\s*(?![^()]*\))/);
 //     //scrape options
 //     //const options = $('.prodwebcode').map((index, element) => $(element).text().trim()).get();
-
 //     //scrape vitamins
 //     const vitamins: [string, number][] = [];
 //     $(".nfvitleft, .nfvitright").map((index, element) => {
@@ -191,7 +139,6 @@ export function scrapeHours() {
 //         parseInt($(element).find(".nfvitpct").text().trim().slice(0, -1)),
 //       ]);
 //     });
-
 //     const macros: macro[] = [];
 //     $(".nfnutrient").map((index, element) => {
 //       const dailyValue = parseInt($(element).find(".nfdvvalnum").text().trim());
@@ -207,7 +154,6 @@ export function scrapeHours() {
 //       };
 //       macros.push(macro);
 //     });
-
 //     const nutrition: NutritionalInfo = {
 //       servingSize: $(".nfserv").text().slice(13).trim(),
 //       caloriesPerServing: $(".nfcal").text().slice(9).trim(),
@@ -238,9 +184,7 @@ export function scrapeHours() {
 //     throw error;
 //   }
 // }
-
 scrapeHours();
 // scrapeMenu(1);
 //scrapeNutrition("http://menu.dining.ucla.edu/Recipes/979336/1"); // for debugging
-
 //checking for completion
