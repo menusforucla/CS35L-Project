fe4e95c0463bacd30b86ac59622b89bb3ba5289e
Seif <seifaziz10@gmail.com>
2023-12-05 14:24:56 -0800

    Update scraper to allow for upserting items

--- packages/scraper/index.ts
+++ packages/scraper/index.ts
@@ -1,16 +1,29 @@
-import axios, { AxiosError } from "axios";
+import type { AxiosError, AxiosResponse } from "axios";
+import axios from "axios";
 import * as cheerio from "cheerio";
 import { z } from "zod";
 
+import type { MealType, MenuItem, MenuSection } from "@menus-for-ucla/db";
 import {
   CarbonFootprint,
   DietaryPreferenceEnum,
   FoodAllergen,
+  prisma,
 } from "@menus-for-ucla/db";
 
-const hoursURL = "http://menu.dining.ucla.edu/Hours";
+const hoursUrl = "http://menu.dining.ucla.edu/Hours";
 
-const diningURLs: string[] = [
+const intervalsForFunctions = {
+  scrapeMenus: 24 * 60 * 60 * 1000, // once a day
+  scrapeActivityLevels: 60 * 60 * 1000, // every 5 minutes
+  scrapeHours: 24 * 60 * 60 * 1000, // once a day
+};
+
+const menuUrl = "http://menu.dining.ucla.edu/Menus";
+const restaurantNames: string[] = ["DeNeve", "BruinPlate", "Epicuria"];
+const mealTypes = ["Breakfast", "Lunch", "Dinner"];
+
+const restaurantUrls: string[] = [
   "http://menu.dining.ucla.edu/Menus/DeNeve/Breakfast",
   "http://menu.dining.ucla.edu/Menus/DeNeve/Lunch",
   "http://menu.dining.ucla.edu/Menus/DeNeve/Dinner",
@@ -53,13 +66,11 @@ const MenuSectionSchema = z.object({
   menuItems: z.array(MenuItemSchema),
 });
 
-const RestaurantsSchema = z.array(
-  z.object({
-    name: z.string(),
-    menuSections: z.array(MenuSectionSchema),
-    isResidentialRestaurant: z.boolean(),
-  }),
-);
+const RestaurantsSchema = z.object({
+  name: z.string(),
+  menuSections: z.array(MenuSectionSchema),
+  isResidentialRestaurant: z.boolean(),
+});
 
 const ActivityLevelsSchema = z.array(
   z.object({
@@ -69,16 +80,9 @@ const ActivityLevelsSchema = z.array(
 );
 
 interface ActivityData {
-  name: string;
-  percentage: number;
+  restaurantName: string;
+  activityLevel: number;
 }
-
-type MenuItem = {
-  name: string;
-  section: string;
-  link: string;
-  nutrition: NutritionalInfo;
-};
 
 interface NutritionalInfo {
   servingSize: string;
@@ -97,150 +101,271 @@ interface macro {
   indented: boolean;
 }
 
-//scrape dining hall availability
-export function scrapeHours() {
+function scrapeActivityLevels() {
   axios
-    .get(hoursURL)
+    .get(hoursUrl)
     .then((response) => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       const $ = cheerio.load(response.data);
       const activityData: ActivityData[] = [];
 
       $("table.hours-table tr").map((index, element) => {
-        const name = $(element).find("span.hours-location").text().trim();
+        const restaurantName = $(element)
+          .find("span.hours-location")
+          .text()
+          .trim();
         const activity = $(element)
           .find("span.activity-level-box")
           .text()
           .trim();
 
         const percentage = parseInt(activity.slice(0, -1), 10);
-        if (name && !isNaN(percentage)) {
-          activityData.push({ name, percentage });
+        if (
+          ActivityLevelsSchema.safeParse([
+            { activityLevel: percentage, restaurantName: restaurantName },
+          ]).success
+        ) {
+          activityData.push({ restaurantName, activityLevel: percentage });
         }
       });
-
-      console.log(activityData);
+      void updateActivityLevel(activityData);
     })
     .catch((error: AxiosError) => {
-      console.log(`Error fetching data from ${hoursURL}: ${error.message}`);
+      console.log(`Error fetching data from ${hoursUrl}: ${error.message}`);
     });
 }
 
-// async function scrapeMenu(menuOption: number): Promise<void> {
-//   try {
-//     const html: AxiosResponse<string> = await axios.get(diningURLs[menuOption]);
-//     if (html.status !== 200) {
-//       throw new Error(`Error fetching data from ${diningURLs[menuOption]}`);
-//     }
+async function updateActivityLevel(activityData: ActivityData[]) {
+  for (const activityLevel of activityData) {
+    const restaurant = await prisma.restaurant.upsert({
+      where: { name: activityLevel.restaurantName },
+      create: {
+        name: activityLevel.restaurantName,
+        currentActivityLevel: activityLevel.activityLevel,
+      },
+      update: { currentActivityLevel: activityLevel.activityLevel },
+    });
 
-//     const $ = cheerio.load(html.data);
-//     const menuItems: MenuItem[] = [];
-//     const sections = $("li.sect-item").toArray();
-//     for (const section of sections) {
-//       // for each section
-//       const sectionName: string = $(section).text().split("\n")[1].trim();
-//       const items = $(section).find("a.recipelink").toArray();
-//       for (const item of items) {
-//         const menuItem: MenuItem = {
-//           name: $(item).text(),
-//           section: sectionName,
-//           link: String($(item).attr("href")),
-//           nutrition: await scrapeNutrition(String($(item).attr("href"))),
-//         };
-//         menuItems.push(menuItem);
-//       }
-//     }
-//     console.log(menuItems);
-//   } catch (error) {
-//     console.log(
-//       `Error fetching data from ${diningURLs[menuOption]}: ${
-//         (error as AxiosError).message
-//       }`,
-//     );
-//   }
-// }
+    await prisma.activityLevel.create({
+      data: {
+        activityLevel: activityLevel.activityLevel,
+        restaurant: { connect: { id: restaurant.id } },
+      },
+    });
+  }
+}
 
-// async function scrapeNutrition(url: string): Promise<NutritionalInfo> {
-//   try {
-//     const html: AxiosResponse<string> = await axios.get(url);
+async function scrapeMenus() {
+  for (const restaurantName of restaurantNames) {
+    for (const mealType of mealTypes) {
+      const restaurantUrl = `${menuUrl}/${restaurantName}/${mealType}`;
+      await scrapeMenu(restaurantUrl, restaurantName, mealType);
+    }
+  }
+}
 
-//     if (html.status !== 200) {
-//       throw new Error(`Error fetching data from ${url}`);
-//     }
+function scrapeMenu(
+  restaurantUrl: string,
+  restaurantName: string,
+  mealType: string,
+): Promise<void> {
+  return axios
+    .get(restaurantUrl)
+    .then(async (response) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const $ = cheerio.load(response.data);
+      const menuSections: MenuSection[] = [];
+      const sections = $("li.sect-item").toArray();
 
-//     const $ = cheerio.load(html.data);
+      const restaurant = await prisma.restaurant.upsert({
+        where: { name: restaurantName },
+        create: { name: restaurantName, currentActivityLevel: 0 },
+        update: {},
+      });
 
-//     //scrape serving size
-//     //const servingSize = $('.nfserv').text().slice(13).trim();
-//     //scrape calories per serving
-//     //const caloriesPerServing = $('.nfcal').text().slice(9).trim();
-//     //scrape ingredients
-//     //const ingredientsString = $('.ingred_allergen').find('p').first().text().slice(13);
-//     //const ingredients = ingredientsString.split(/\s*,\s*(?![^()]*\))/);
-//     //scrape allergens
-//     //splits ingredients by using a negative lookahead assertion to check for close paren
-//     //const allergens = $('.ingred_allergen > p').eq(1).text().slice(12).split(/\s*,\s*(?![^()]*\))/);
-//     //scrape options
-//     //const options = $('.prodwebcode').map((index, element) => $(element).text().trim()).get();
+      for (const section of sections) {
+        await scrapeMenuSection($, section, mealType, restaurant.id);
+        // menuSections.push(menuSection);
+      }
 
-//     //scrape vitamins
-//     const vitamins: [string, number][] = [];
-//     $(".nfvitleft, .nfvitright").map((index, element) => {
-//       vitamins.push([
-//         $(element).find(".nfvitname").text().trim(),
-//         parseInt($(element).find(".nfvitpct").text().trim().slice(0, -1)),
-//       ]);
-//     });
+      // const restaurant: Restaurant = {
+      //   name: restaurantName,
+      //   isResidentialRestaurant: false,
+      //   menuSections: menuSections,
+      // };
+    })
+    .catch((error: AxiosError) => {
+      console.error(
+        `Error fetching data from ${restaurantUrl}: ${error.message}`,
+      );
+    });
+}
 
-//     const macros: macro[] = [];
-//     $(".nfnutrient").map((index, element) => {
-//       const dailyValue = parseInt($(element).find(".nfdvvalnum").text().trim());
-//       const fullText = $(element).text().split(" ");
-//       const name = fullText.slice(0, -2).join(" ");
-//       const grams = fullText.slice(-2, -1).join(" ");
-//       const indented = $(element).parent(".nfindent").length > 0;
-//       const macro: macro = {
-//         name: name,
-//         grams: grams,
-//         dv: dailyValue,
-//         indented: indented,
-//       };
-//       macros.push(macro);
-//     });
+async function scrapeMenuSection(
+  $: cheerio.CheerioAPI,
+  section: cheerio.Element,
+  mealType: string,
+  restaurantId: number,
+) {
+  const sectionName: string = $(section).text().split("\n")[1].trim();
+  const items = $(section).find("a.recipelink").toArray();
 
-//     const nutrition: NutritionalInfo = {
-//       servingSize: $(".nfserv").text().slice(13).trim(),
-//       caloriesPerServing: $(".nfcal").text().slice(9).trim(),
-//       macronutrients: macros,
-//       vitamins: vitamins,
-//       ingredients: $(".ingred_allergen")
-//         .find("p")
-//         .first()
-//         .text()
-//         .slice(13)
-//         .split(/\s*,\s*(?![^()]*\))/),
-//       allergens: $(".ingred_allergen > p")
-//         .eq(1)
-//         .text()
-//         .slice(12)
-//         .split(/\s*,\s*(?![^()]*\))/),
-//       options: $(".prodwebcode")
-//         .map((index, element) => $(element).text().trim())
-//         .get(),
-//     };
-//     return nutrition;
-//   } catch (error) {
-//     console.error(
-//       `Error fetching nutritional information from ${url}: ${
-//         (error as AxiosError).message
-//       }`,
-//     );
-//     throw error;
-//   }
-// }
+  const menuSection = await prisma.menuSection.upsert({
+    where: {
+      menuSectionIdentifier: {
+        name: section.name,
+        restaurantId: restaurantId,
+      },
+    },
+    update: {},
+    create: {
+      name: section.name,
+      restaurant: { connect: { id: restaurantId } },
+      menuItems: { create: [] },
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      mealType: mealType.toUpperCase() as MealType,
+    },
+  });
 
-scrapeHours();
+  for (const item of items) {
+    const url = $(item).attr("href");
+    await scrapeMenuItem(url!, menuSection.id);
+    // menuItems.push(menuItem);
+  }
+
+  // const menuSection: MenuSection = {
+  //   name: sectionName,
+  //   mealType: mealType.toUpperCase() as MealType,
+  //   menuItems: menuItems,
+  // };
+  // return menuSection;
+}
+
+async function scrapeMenuItem(url: string, menuSectionId: number) {
+  try {
+    const html: AxiosResponse<string> = await axios.get(url);
+    if (html.status !== 200) {
+      throw new Error(`Error fetching data from ${url}`);
+    }
+
+    const $ = cheerio.load(html.data);
+
+    //scrape vitamins
+    const vitamins: [string, number][] = [];
+    $(".nfvitleft, .nfvitright").map((index, element) => {
+      vitamins.push([
+        $(element).find(".nfvitname").text().trim(),
+        parseInt($(element).find(".nfvitpct").text().trim().slice(0, -1)),
+      ]);
+    });
+
+    const macros: macro[] = [];
+    $(".nfnutrient").map((index, element) => {
+      const dailyValue = parseInt($(element).find(".nfdvvalnum").text().trim());
+      const fullText = $(element).text().split(" ");
+      const name = fullText.slice(0, -2).join(" ");
+      const grams = fullText.slice(-2, -1).join(" ");
+      const indented = $(element).parent(".nfindent").length > 0;
+      const macro: macro = {
+        name: name,
+        grams: grams,
+        dv: dailyValue,
+        indented: indented,
+      };
+      macros.push(macro);
+    });
+
+    const nutrition: NutritionalInfo = {
+      servingSize: $(".nfserv").text().slice(13).trim(),
+      caloriesPerServing: $(".nfcal").text().slice(9).trim(),
+      macronutrients: macros,
+      vitamins: vitamins,
+      options: $(".prodwebcode")
+        .map((index, element) => $(element).text().trim())
+        .get(),
+    };
+    console.log(nutrition);
+
+    const rawAllerges: string[] = $(".ingred_allergen > p")
+      .eq(1)
+      .text()
+      .slice(12)
+      .split(/\s*,\s*(?![^()]*\))/);
+
+    const allergens: FoodAllergen[] = [];
+    for (const rawAllergen of rawAllerges) {
+      allergens.push(FoodAllergen[rawAllergen as keyof typeof FoodAllergen]);
+    }
+    const allergenIds = await getAllergenIds(allergens);
+
+    const menuItem: MenuItem = {
+      name: $("h2").text().trim(),
+      description: $(".recipe_description").text().trim(),
+      ingredients: $(".ingred_allergen").find("p").first().text(),
+      carbonFootprint: CarbonFootprint.LOW,
+    };
+
+    upsertMenuItem(menuItem)
+  } catch (error) {
+    console.error(
+      `Error fetching nutritional information from ${url}: ${
+        (error as AxiosError).message
+      }`,
+    );
+    throw error;
+  }
+}
+
+async function getAllergenIds(allergens: FoodAllergen[]): Promise<number[]> {
+  return Promise.all(
+    allergens.map(async (allergenName) => {
+      const allergen = await prisma.allergen.upsert({
+        where: { name: allergenName },
+        update: {},
+        create: { name: allergenName },
+      });
+      return allergen.id;
+    }),
+  );
+}
+
+// scrapeActivityLevels();
+void scrapeMenus();
+
 // scrapeMenu(1);
 //scrapeNutrition("http://menu.dining.ucla.edu/Recipes/979336/1"); // for debugging
 
 //checking for completion
+
+async function upsertMenuItem(
+  item: z.infer<typeof MenuItemSchema>,
+  menuSectionId: number,
+  allergenIds: number[],
+  dietaryPreferenceIds: number[],
+) {
+  await prisma.menuItem.upsert({
+    where: { menuItemIdentifier: { name: item.name, menuSectionId } },
+    update: {
+      description: item.description,
+      ingredients: item.ingredients,
+      allergens: { connect: allergenIds.map((id) => ({ id })) },
+      carbonFootprint: item.carbonFootprint,
+      dietaryPreferences: {
+        connect: dietaryPreferenceIds.map((id) => ({ id })),
+      },
+      nutritionFacts: { update: item.nutritionFacts },
+    },
+    create: {
+      name: item.name,
+      description: item.description,
+      ingredients: item.ingredients,
+      allergens: { connect: allergenIds.map((id) => ({ id })) },
+      carbonFootprint: item.carbonFootprint,
+      dietaryPreferences: {
+        connect: dietaryPreferenceIds.map((id) => ({ id })),
+      },
+      nutritionFacts: { create: item.nutritionFacts },
+      menuSection: { connect: { id: menuSectionId } },
+    },
+  });
+}
